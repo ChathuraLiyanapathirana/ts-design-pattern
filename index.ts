cf50fcc2192c51b98d211b97475dd91629140126
ChathuraLiyanapathirana <cbuddhika3@gmail.com>
2021-06-08 18:02:14 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,22 +1,6 @@
-class Singleton {
-  private static instance: Singleton;
+// Import stylesheets
+import './style.css';
 
-  private constructor() {}
-
-  public static getInstance(): Singleton {
-    if (!Singleton.instance) {
-      Singleton.instance = new Singleton();
-    }
-    return Singleton.instance;
-  }
-}
-
-const testFunction = () => {
-  const instance1 = Singleton.getInstance();
-  const instance2 = Singleton.getInstance();
-
-  const appDiv: HTMLElement = document.getElementById('app');
-  appDiv.innerHTML = `(instance1 === instance2) -> ${instance1 === instance2}`;
-};
-
-testFunction();
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
